74a3bfe1b177c5467f194b6e3566bddba0d103aa
Antoine31G <116873215+Antoine31G@users.noreply.github.com>
2023-04-20 10:52:16 +0200

    Jeu presque final

--- Code/Games/TirAuBallon/GameExemple.c
+++ Code/Games/TirAuBallon/GameExemple.c
@@ -27,6 +27,55 @@ void Exemple_Create(PGAME _pExemple)
     pGameData->image[18]= al_load_bitmap(PATH"\\ticket.png");
     pGameData->image[19]= al_load_bitmap(PATH"\\chiffreun.png");
     pGameData->image[20]= al_load_bitmap(PATH"\\chiffrezero.png");
+    pGameData->image[21]= al_load_bitmap(PATH"\\boutonscore.png");
+    pGameData->image[22]= al_load_bitmap(PATH"\\fondscore.jpg");
+    pGameData->image[23]= al_load_bitmap(PATH"\\croix.png");
+    /*
+    pGameData->image[24]= al_load_bitmap(PATH"\\hit1.png");
+    pGameData->image[25]= al_load_bitmap(PATH"\\hit2.png");
+    pGameData->image[26]= al_load_bitmap(PATH"\\hit3.png");
+    pGameData->image[27]= al_load_bitmap(PATH"\\hit4.png");
+    pGameData->image[28]= al_load_bitmap(PATH"\\hit5.png");
+    pGameData->image[29]= al_load_bitmap(PATH"\\hit6.png");
+    pGameData->image[30]= al_load_bitmap(PATH"\\hit7.png");
+    pGameData->image[31]= al_load_bitmap(PATH"\\hit8.png");
+    pGameData->image[32]= al_load_bitmap(PATH"\\hit9.png");
+    pGameData->image[33]= al_load_bitmap(PATH"\\hit10.png");
+    pGameData->image[34]= al_load_bitmap(PATH"\\hit11.png");
+    pGameData->image[35]= al_load_bitmap(PATH"\\hit12.png");
+    pGameData->image[36]= al_load_bitmap(PATH"\\hit13.png");
+    pGameData->image[37]= al_load_bitmap(PATH"\\hit14.png");
+    pGameData->image[38]= al_load_bitmap(PATH"\\hit15.png");
+    pGameData->image[39]= al_load_bitmap(PATH"\\hit16.png");
+    pGameData->image[40]= al_load_bitmap(PATH"\\hit17.png");
+    pGameData->image[41]= al_load_bitmap(PATH"\\hit18.png");
+    pGameData->image[42]= al_load_bitmap(PATH"\\hit19.png");
+    pGameData->image[43]= al_load_bitmap(PATH"\\hit20.png");
+    pGameData->image[44]= al_load_bitmap(PATH"\\hit21.png");
+    pGameData->image[45]= al_load_bitmap(PATH"\\hit22.png");
+    pGameData->image[46]= al_load_bitmap(PATH"\\hit23.png");
+    pGameData->image[47]= al_load_bitmap(PATH"\\hit24.png");
+    pGameData->image[48]= al_load_bitmap(PATH"\\hit25.png");
+    pGameData->image[49]= al_load_bitmap(PATH"\\hit26.png");
+    pGameData->image[50]= al_load_bitmap(PATH"\\hit27.png");
+    pGameData->image[51]= al_load_bitmap(PATH"\\hit28.png");
+    pGameData->image[52]= al_load_bitmap(PATH"\\hit29.png");
+    pGameData->image[53]= al_load_bitmap(PATH"\\hit30.png");
+    pGameData->image[54]= al_load_bitmap(PATH"\\hit31.png");
+    pGameData->image[55]= al_load_bitmap(PATH"\\hit32.png");
+    pGameData->image[56]= al_load_bitmap(PATH"\\hit33.png");
+    pGameData->image[57]= al_load_bitmap(PATH"\\hit34.png");
+    pGameData->image[58]= al_load_bitmap(PATH"\\hit35.png");
+    pGameData->image[59]= al_load_bitmap(PATH"\\hit36.png");
+    pGameData->image[60]= al_load_bitmap(PATH"\\hit37.png");
+    pGameData->image[61]= al_load_bitmap(PATH"\\hit38.png");
+    pGameData->image[62]= al_load_bitmap(PATH"\\hit39.png");
+    pGameData->image[63]= al_load_bitmap(PATH"\\hit40.png");
+    pGameData->image[64]= al_load_bitmap(PATH"\\hit41.png");
+    pGameData->image[65]= al_load_bitmap(PATH"\\hit42.png");
+    pGameData->image[66]= al_load_bitmap(PATH"\\hit43.png");
+    pGameData->image[67]= al_load_bitmap(PATH"\\hit44.png");
+    pGameData->image[68]= al_load_bitmap(PATH"\\hit45.png");*/
     pGameData->gamemode = 0;
     pGameData->nbballon = 90;
     pGameData->compteurvaisseau=2000;
@@ -46,11 +95,12 @@ void Exemple_Create(PGAME _pExemple)
     pGameData->compteurdroite=0;
     pGameData->ballondetruit=0;
     pGameData->timeur = 0;
-    pGameData->tempsrestant = 60;
+    pGameData->tempsrestant = 30;
     pGameData->tourfait=0;
     pGameData->ticketJ2=0;
     pGameData->ticketJ1=0;
     pGameData->compteursauvegarde=0;
+    pGameData->compteurimage=24;
     for (int i=0;i<pGameData->nbballon+1;i++){
         if (i%2 == 1){
             pGameData->pballon[i].vx=2;
@@ -326,7 +376,7 @@ void reset(PGAME _pExemple){
     pGameData->compteurdroite=0;
     pGameData->ballondetruit=0;
     pGameData->timeur = 0;
-    pGameData->tempsrestant = 60;
+    pGameData->tempsrestant = 30;
 }
 
 
@@ -401,6 +451,7 @@ void Exemple_TimedUpdate(PGAME _pExemple)
         }
     }
     if (pGameData->gamemode==1){
+        int a=0;
         al_draw_bitmap(pGameData->image[3],0,0,0);
         ALLEGRO_DISPLAY*ecran= al_get_current_display();
         al_hide_mouse_cursor(ecran);
@@ -462,6 +513,19 @@ void Exemple_TimedUpdate(PGAME _pExemple)
             pGameData->gamemode=3;
             pGameData->pointJ2=pGameData->ballondetruit;
         }
+        /*if (pGameData->click == 2){   //regler probleme animation                         // SI LE JOUEUR CASSE TOUT LES BALLONS AVANT LE TEMPS
+            for (int i=0;i<10*45;i++){
+                if (i%10 == 1){
+                    pGameData->compteurimage++;
+                    if (pGameData->compteurimage >= 67){
+                        pGameData->compteurimage=67;
+                    }
+                }
+                al_draw_bitmap(pGameData->image[pGameData->compteurimage],500,500,0);
+            }
+            pGameData->click=0;
+            pGameData->compteurimage=24;
+        }*/
     }
     if (pGameData->gamemode==2){
         ALLEGRO_DISPLAY * ecran=al_get_current_display();
@@ -484,12 +548,21 @@ void Exemple_TimedUpdate(PGAME _pExemple)
     }
     if (pGameData->gamemode==3){
         al_draw_bitmap(pGameData->image[15],0,0,0);
+        al_draw_bitmap(pGameData->image[21],350,900,0);
+        al_draw_bitmap(pGameData->image[23],1800,0,0);
+        if (Point_In_Rectangle(pGameData->mouse, (Vector2D){1800,0}, (Vector2D){1872,82}) == 1 && pGameData->click==1){
+            pGameData->click=0;
+            pGameData->gamemode=5;
+        }
+        if (Point_In_Rectangle(pGameData->mouse, (Vector2D){350,900}, (Vector2D){639,989}) == 1 && pGameData->click==1){
+            pGameData->gamemode=4;
+            pGameData->click=0;
+        }
         FILE* fichier=NULL;
         fichier= fopen(PATH"\\sauvegarde.txt","r");
         fscanf(fichier,"%d\n%d\n%d\n",&pGameData->meilleurscore1,&pGameData->meilleurscore2,&pGameData->meilleurscore3);
         fclose(fichier);
         if (pGameData->compteursauvegarde==0){
-            printf("aa");
             traitementmeilleurscore(_pExemple);
             pGameData->compteursauvegarde=1;
         }
@@ -551,8 +624,26 @@ void Exemple_TimedUpdate(PGAME _pExemple)
             pGameData->ticketJ2=1;
         }
     }
+    if (pGameData->gamemode==4){
+        al_draw_bitmap(pGameData->image[22],0,0,0);
+        al_draw_bitmap(pGameData->image[17],500,-100,0);
+        al_draw_bitmap(pGameData->image[23],1800,0,0);
+        sprintf(pGameData->score1,"%d",pGameData->meilleurscore1);
+        sprintf(pGameData->score2,"%d",pGameData->meilleurscore2);
+        sprintf(pGameData->score3,"%d",pGameData->meilleurscore3);
+        al_draw_text(pGameData->police[1], al_map_rgb(0,0,0),1080,520,0,pGameData->score1);
+        al_draw_text(pGameData->police[1], al_map_rgb(0,0,0),1080,670,0,pGameData->score2);
+        al_draw_text(pGameData->police[1], al_map_rgb(0,0,0),1080,820,0,pGameData->score3);
+        if (Point_In_Rectangle(pGameData->mouse, (Vector2D){1800,0}, (Vector2D){1872,82}) == 1 && pGameData->click==1){
+            pGameData->click=0;
+            pGameData->gamemode=3;
+        }
+    }
+    if (pGameData->gamemode==5){
+        exit(0);
+    }
 }
-///////////////////////////////////////////////////FAIRE SAUVEGARDE SCORE  // ANIMATION BALLON DETRUIT
+//////////////////////////////////////////////////   // ANIMATION BALLON DETRUIT
 void Exemple_Destroy(PGAME _pExemple)
 {
     printf("Destruction du jeu...\n");
