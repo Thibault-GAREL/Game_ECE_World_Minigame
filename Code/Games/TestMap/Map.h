d1fa45196b9472d20287df81faf43b06b6a767f4
Antoine31G <antoine31.goudedranche@gmail.com>
2023-04-23 16:14:24 +0200

    Test map premier push structure

---
+++ Code/Games/TestMap/Map.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Game.h"
+#include "allegroutils.h"
+#include "vector.h"
+
+#define CL_PATH ".."
+#define PATH CL_PATH
+
+
+typedef struct _IMAGES
+{
+    float x;
+    float y;
+}Images;
+
+
+typedef struct _MAPDATA
+{
+    Vector2D mouse;
+    int click;
+    ALLEGRO_BITMAP* image[15];
+    Images* pimages;
+    float x;
+    float y;
+    int nbimages;
+    int deplacementhori;
+    int deplacementverti;
+    float speedhori;
+    float speedverti;
+    float bonhommex;
+    float bonhommey;
+}MapData;
+
+
+
+void Map_Create(PMAP _pMap);
+void Map_Update(PMAP _pMap);
+void gestionbordure(PMAP _pMap);
+void rect(PMAP _pMap,float x,float y,float taillex,float tailley,int i);
+void gestioncolision(PMAP _pMap);
+void Map_TimedUpdate(PMAP _pMap);
+void Map_Destroy(PMAP _pMap);
+
