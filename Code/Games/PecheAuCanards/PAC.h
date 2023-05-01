ff90575912aa60a808d1c5b4418112fcb54acda0
Robin Koenig <robin.koenig@edu.ece.fr>
2023-05-01 14:45:58 +0200

    PAC game is done, waiting for menu and audio

--- Code/Games/PecheAuCanards/PAC.h
+++ Code/Games/PecheAuCanards/PAC.h
@@ -11,25 +11,40 @@
 typedef struct _DuckInfos{
     float x, y;
     float vx, vy;
-    int click;
+    char KEYCodeDuck;
+    int Points;
 }DuckInfos, *pDuckInfos;
 
 typedef struct _GAMEDATA
 {
-    ALLEGRO_BITMAP* textures[15];
-    ALLEGRO_FONT* police;
+    ALLEGRO_FONT* font;
+    ALLEGRO_COLOR fontColor;
     DuckInfos *DuckInfos[DuckCount];
     ALLEGRO_BITMAP *DuckTextures[DuckCount];
     ALLEGRO_BITMAP *background;
+    ALLEGRO_BITMAP *End;
     Vector2D mouse;
     int click;
     int Fishingstate[DuckCount];
+    int score1;
+    char Score1[100];
+    int score2;
+    char Score2[100];
+    char KeyPress[21];
+    int CurrentDuckFished;
+    int timer;
+    int PlayerID;
+    int Player1_Timer;
+    int Player2_Timer;
+    char RemainingTime[100];
 }PacGameData, *pPacGameData;
 
-void PAC_Create(PGAME _pExemple);
-void PAC_Update(PGAME _pExemple);
-void PAC_TimedUpdate(PGAME _pExemple);
-void PAC_Destroy(PGAME _pExemple);
+void PAC_Create(PGAME _pPAC);
+void PAC_Update(PGAME _pPAC);
+void PAC_Reinit(PGAME _pPAC);
+void PAC_TimedUpdate(PGAME _pPAC);
+void PAC_Destroy(PGAME _pPAC);
 void PAC_Coordinates_create(PGAME _pPAC);
-void Check_Duck_Colisions(PGAME _pPAC);
-void Check_Click_on_Duck();
+void Check_Duck_Collisions(PGAME _pPAC);
+void Check_Click_on_Duck(PGAME _pPAC);
+void Was_Key_Pressed(PGAME _pPAC, int DuckID);
