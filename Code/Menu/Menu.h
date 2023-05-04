4a3730263d933cc7956409590f1862fae7660801
Robin Koenig <robin.koenig@edu.ece.fr>
2023-05-04 23:32:57 +0200

    Menu is almost done with the possibility to choose name (there's still an error which causes multiple touch)

--- Code/Menu/Menu.h
+++ Code/Menu/Menu.h
@@ -5,11 +5,27 @@
 #include "../Game.h"
 #include "..\\Utils\\Vector.h"
 
+typedef struct _MenuBitmap{
+    int x;
+    int y;
+    int vx;
+    int vy;
+    int MouseIsOver;
+}MenuBitmapInfo, *pMenuBitmapInfo;
+
 typedef struct _MenuGameData {
     Vector2D mouse;
     int click;
     int GameLaunched;
     int Main_Menu_Select;
+
+    char GetName1[256];
+    int CurrentCharPos;
+    ALLEGRO_FONT *GetNameFont;
+    int GetNameState;
+    ALLEGRO_KEYBOARD_STATE keyboard_state;
+
+    pMenuBitmapInfo MB_Infos[10];
     ALLEGRO_BITMAP *Menu_Bitmap[10];
 }MenuGameData, *pMenuGameData;
 
@@ -17,3 +33,4 @@ void Menu_Create(PGAME _pMenu);
 void Menu_Update(PGAME _pMenu);
 void Menu_TimedUpdate(PGAME _pMenu);
 void Menu_Destroy(PGAME _pMenu);
+void Get_Player_Name(PGAME _pMenu);
