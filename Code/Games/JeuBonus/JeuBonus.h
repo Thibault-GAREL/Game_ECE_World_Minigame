78cdfc3b9b0e4cf128969e592b732b95be33b3b8
Antoine31G <antoine31.goudedranche@gmail.com>
2023-05-05 21:59:38 +0200

    JeuBonus premiere avancée

--- Code/Games/JeuBonus/JeuBonus.h
+++ Code/Games/JeuBonus/JeuBonus.h
@@ -14,11 +14,40 @@ typedef struct _BONUSDATA
 {
     Vector2D mouse;
     int click;
+    ALLEGRO_BITMAP* image[30];
+    float x1;
+    float y1;
+    float x2;
+    float y2;
+    float xballon;
+    float yballon;
+    int sensbonhomme1;
+    int sensbonhomme2;
+    int deplacementx1;
+    int deplacementx2;
+    int compteursautJ1;
+    int compteursautJ2;
+    int compteurfinsautJ1;
+    int compteurfinsautJ2;
+    int eligibilitesautJ1;
+    int eligibilitesautJ2;
+    int tirJ1;
+    int tirJ2;
+    int compteuranimJ1;
+    int compteuranimJ2;
+    int compteurfinanimJ1;
+    int compteurfinanimJ2;
+    int a;
+    int b;
+    int c;
+    int d;
 }BonusData;
 
 
 void Bonus_Create(PGAME _pBonus);
 void Bonus_Update(PGAME _pBonus);
+void gestionbordurebonhommes(PGAME _pBonus);
+void affichagebonhommes(PGAME _pBonus);
 void Bonus_TimedUpdate(PGAME _pBonus);
 void Bonus_Destroy(PGAME _pBonus);
 
